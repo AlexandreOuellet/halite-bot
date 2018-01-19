63026a85ecc4222de3c6847949b7f5e3c80ed9a9
Alexandre Ouellet <AOuellet@square-enix-montreal.com>
2018-01-19 15:37:30 -0500

    run_game is modified

--- run_game.bat
+++ run_game.bat
@@ -1,8 +1,16 @@
 del *.log /Q
 del *.hlt /Q
 
-FOR /L %%A IN (1,1,250) DO (
-  halite.exe -d "240 160" "python MyBot.py G1" "python MyBot.py"
+REM FOR /L %%A IN (1,1,100) DO (
+REM   halite.exe -d "240 160" "python MyBot.py G2" "python MyBot.py G2"
+REM   REM ping localhost -n 1
+REM   REM python train.py
+REM )
+
+REM python train.py
+
+FOR /L %%A IN (1,1,1000) DO (
+  halite.exe -d "240 160" "python MyBot.py G2 False" "python MyBot.py G2"
   REM ping localhost -n 1
-  REM python train.py
+  python train.py
 )
