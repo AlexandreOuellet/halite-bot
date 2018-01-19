a0de82677ea598a6e00f11fad7d7db8a40a6459f
Alexandre Ouellet <AOuellet@square-enix-montreal.com>
2018-01-19 16:01:38 -0500

    better run_game.bat

--- run_game.bat
+++ run_game.bat
@@ -9,8 +9,14 @@ REM )
 
 REM python train.py
 
-FOR /L %%A IN (1,1,1000) DO (
-  halite.exe -d "240 160" "python MyBot.py G2 False" "python MyBot.py G2"
+FOR /L %%A IN (1,1,20) DO (
+  halite.exe -d "240 160" "python MyBot.py G2" "python MyBot.py G2"
   REM ping localhost -n 1
   python train.py
+)
+
+FOR /L %%A IN (1,1,20000) DO (
+  halite.exe -d "240 160" "python MyBot.py G2" "python MyBot.py G2"
+  REM ping localhost -n 1
+  python train.py True
 )
