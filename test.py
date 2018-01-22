ce909d636ba2d290eb4c1f5790cc7986ef733804
Alexandre Ouellet <AOuellet@square-enix-montreal.com>
2018-01-22 13:43:15 -0500

    Coefficient usage instead of Neural Networks

--- test.py
+++ test.py
@@ -1,13 +1,3 @@
-import numpy as np
-import nnutils
-# import keras
-
-index = nnutils.parseCommandToActionIndex('t 0 7 90')
-
-# rand = np.random.randint(10, size=(100, 1))
-# # y_train = keras.utils.to_categorical(, num_classes=10)
-# print(rand)
-
-# import keras
-# y_train = keras.utils.to_categorical(rand, num_classes=10)
-# print(y_train)
+import operator
+x = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
+sorted_x = sorted(x.items(), key=operator.itemgetter(1))
